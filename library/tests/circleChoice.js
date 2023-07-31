9cf89660659a4bbd7bda73a6108808a07ad6a94a
Vivian Kuo <vivian.kuo4@outlook.com>
2023-07-30 18:54:50 -1000

    added to js of all shape choices

--- library/tests/circleChoice.js
+++ library/tests/circleChoice.js
@@ -7,9 +7,10 @@ class circleChoice extends Shape {
     render() {
         return `
         <svg version="1.1"
-            width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="150" cy="100" r="20" fill=${this.color} />
-            <<text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+            width="300" height="200" 
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="150" cy="100" r="20" fill=${this.shapeColor} />
+            <text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
         </svg>
         `
     };
--- library/tests/squareChoice.js
+++ library/tests/squareChoice.js
@@ -7,7 +7,8 @@ class squareChoice extends Shape {
     render() {
         return `
         <svg version"1.1"
-            width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+            width="200" height="200" 
+            xmlns="http://www.w3.org/2000/svg">
             <rect width="200" height="200" fill="${this.shapeColor}"/>
             <text x="100" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
         </svg>
