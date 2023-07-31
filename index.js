92f4f3c26346fb55da3d6df901effde1259e4b15
Vivian Kuo <vivian.kuo4@outlook.com>
2023-07-30 18:30:00 -1000

    updated

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const inquirer = require('inquirer');
 const { Circle, Triangle, Square } = require('./library/tests/shapes');
-
-const fs = require ('fs');
 const questions = require (`./library/tests/questions.js`);
+const fs = require ('fs');
 const fileName = "./examples/logo.svg";
-const setShape = require('./library/tests/createShape.js')
+const createShape = require('./library/tests/createShape.js')
 
 
 function generateLogo(response) {
-    const svg = setShape (response);
+    const svg = createShape (response);
     fs.writeFile(fileName, svg, ()=> console.log(`Generated logo.svg`));
 }
 
-function init(){
+function init() {
     inquirer
     .prompt(questions)
     .then((response) => {
